e31ec24f61129e4ea717f69a8808336b6361daa8
Rachel <rslim87@gmail.com>
2017-09-27 13:21:24 -0400

    updated cors.rb

--- db/migrate/20170927162244_create_users.rb
+++ db/migrate/20170927162244_create_users.rb
@@ -3,7 +3,9 @@ class CreateUsers < ActiveRecord::Migration[5.1]
     create_table :users do |t|
       t.string :username
       t.string :email
+      t.string :password_digest
       t.boolean :valid_passport
+
 
       t.timestamps
     end
