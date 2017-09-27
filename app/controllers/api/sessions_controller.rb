90fd88ad3a5fe110b9ef731800cdbfa0da01e32d
Rachel <rslim87@gmail.com>
2017-09-27 18:12:46 -0400

    added create section to sessions controller

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,18 +1,23 @@
 class Api::SessionsController < ApplicationController
 
-	# skip_before_action :authenticate
+	skip_before_action :authenticate
 
-	# def create
-	# 	user = User.find_by(email: auth_params[:email])
-	# 	if user.authenticate(auth_params[:password])
-	# 		jwt = Auth.issue({ user: user.id })
-	# 		render json: { jwt: jwt }
-	# 	end
-	# end
+	def create
+		user = User.find_by(email: auth_params[:email])
+		if user.authenticate(auth_params[:password])
+			jwt = Auth.issue({ user: user.id })
+			render json: { jwt: jwt }
+		end
+	end
 
-	# private
+	def destroy 
+		@auth_token = @current_user.authentication_tokens.find(params[:id])
+		@auth_token.destroy!
+	end
 
-	# 	def auth_params
-	# 		params.require(:auth).permit(:email, :password)
-	# 	end
+	private
+
+		def auth_params
+			params.require(:auth).permit(:email, :password)
+		end
 end
