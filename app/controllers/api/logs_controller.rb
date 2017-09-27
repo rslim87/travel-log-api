1eaec0c0c14a966c95194bfc12da6752af31b36a
Rachel <rslim87@gmail.com>
2017-09-27 18:26:14 -0400

    added index, create, show, update, and destroy action to logs controller

--- app/controllers/api/logs_controller.rb
+++ app/controllers/api/logs_controller.rb
@@ -3,5 +3,37 @@ class Api::LogsController < ApplicationController
 	def index
 		render json: Log.all
 	end
-	
+
+	def create
+		log = Log.new(log_params)
+		if log.save
+			render json: log
+		end
+	end
+
+	def show
+		@log = Log.find_by(id: params[:id])
+		render json: @log
+	end
+
+	def update
+		@log = Log.find_by(id: params[:id])
+		if @log.update(log_params)
+			render json: @log
+		end
+	end
+
+	def destroy
+		@log = Log.find_by(id: params[:id])
+		if @log.destroy
+			render json: {message: 'successfully destroyed'}
+		end
+	end
+
+	private
+
+	def log_params
+		params.require(:log).permit(:city, :country, :solo_travel, :month, :year)
+	end
+
 end
